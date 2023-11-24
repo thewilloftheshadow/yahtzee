6883da00a2099213efd721cccae0e82d7fc670b5
Shadow <shadow@buape.com>
2023-11-24 00:32:06 -0600

    all debug

--- commands/gameInfo.ts
+++ commands/gameInfo.ts
@@ -1,14 +1,33 @@
-import { Component, CrossBuild, ReceivedInteraction } from "crossbuild"
+import {
+	Component,
+	CrossBuild,
+	ReceivedInteraction,
+	OptionsHandler,
+} from "crossbuild"
 import { games } from "../index.js"
 
 export default class Cmd extends Component {
 	constructor(client: CrossBuild) {
 		super("game-info", "command", client, {
 			description: "Debug command to show all game data!",
+			options: [
+				{
+					type: "boolean",
+					name: "all",
+					description: "Show all games, including ended ones",
+				},
+			],
 		})
 	}
 
-	override async run(interaction: ReceivedInteraction) {
+	override async run(
+		interaction: ReceivedInteraction,
+		options: OptionsHandler
+	) {
+		if (options.getBoolean("all") === true)
+			return interaction.reply({
+				content: JSON.stringify(games),
+			})
 		return interaction.reply({
 			content: JSON.stringify(games.filter((x) => x.status !== "ended")),
 		})
